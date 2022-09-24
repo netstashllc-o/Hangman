d64db715820fb8c295dd16b4b5728edfdc80b738
Meg Prescott <mprescott@ccsnh.edu>
2022-09-24 11:08:18 -0400

    Project complete

--- Hangman/task/main.js
+++ Hangman/task/main.js
@@ -1,7 +1,12 @@
 'use strict';
-const input = require('sync-input')
+const input = require('sync-input');
 const wordList = ['python', 'java', 'swift', 'javascript'];
+const maxAttempts = 8;
+let wins = 0;
+let losses = 0;
+
 const getRandomWord = (words) => words[Math.floor(Math.random() * words.length)];
+
 const mapLettersInWord = (word) => {
     let charsInWord = new Map();
     for (let i = 0; i < word.length; i++) {
@@ -13,50 +18,85 @@ const mapLettersInWord = (word) => {
         }
     }
     return charsInWord;
-}
-let attempts = 8;
+};
 
-console.log(`H A N G M A N # ${attempts} attempts\n`);
-let answer = getRandomWord(wordList).toLowerCase();
-let guesses = answer.split('').fill('-', 0, answer.length);
-let answerMap = mapLettersInWord(answer);
-let misses = [];
+const playGame = () => {
+    let answer = getRandomWord(wordList).toLowerCase();
+    let guesses = answer.split('').fill('-', 0, answer.length);
+    let answerMap = mapLettersInWord(answer);
+    let misses = [];
+    let attempts = maxAttempts;
 
-while (attempts > 0 && guesses.includes('-')) {
-    console.log(guesses.join(''));
-    let guess = input(`Input a letter: `);
+    while (attempts > 0 && guesses.includes('-')) {
+        console.log(guesses.join(''));
+        let guess = input(`Input a letter: `);
 
-    if (!guess || guess.length > 1) {
-        console.log('Please, input a single letter.\n');
-        continue;
+        if (!guess || guess.length > 1) {
+            console.log('Please, input a single letter.\n');
+            continue;
+        }
+
+        if (guess < 'a' || guess > 'z') {
+            console.log('Please, enter a lowercase letter from the English alphabet.\n');
+            continue;
+        }
+
+        if (guesses.includes(guess) || misses.includes(guess)) {
+            console.log(`You've already guessed this letter.\n`);
+            continue;
+        }
+
+        if (!answerMap.has(guess)) {
+            attempts--;
+            console.log(`That letter doesn't appear in the word. # ${attempts} attempts\n`);
+            misses.push(guess);
+            continue;
+        }
+
+        for (let i of answerMap.get(guess)) {
+            guesses[i] = guess;
+        }
+
+        console.log();
     }
 
-    if (guess < 'a' || guess > 'z') {
-        console.log('Please, enter a lowercase letter from the English alphabet.\n');
-        continue;
-    }
+    let lost = guesses.includes('-');
+    console.log(lost ? 'You lost!\n' : `You guessed the word ${answer}!\nYou survived!\n`);
+    wins += (lost) ? 0 : 1;
+    losses += (lost) ? 1 : 0;
+};
 
-    if (guesses.includes(guess) || misses.includes(guess)) {
-        console.log(`You've already guessed this letter.\n`);
-        continue;
-    }
+const showResults = () => {
+    console.log(`You won: ${wins} times.
+You lost: ${losses} times.\n`);
+};
 
-    if (!answerMap.has(guess)) {
-        attempts--;
-        console.log(`That letter doesn't appear in the word. # ${attempts} attempts\n`);
-        misses.push(guess);
-        continue;
-    }
+//Main function
+(function () {
+    let choice;
+    let keepGoing = true;
+    let m = 'Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit: ';
 
-    for (let i of answerMap.get(guess)) {
-        guesses[i] = guess;
-    }
+    console.log(`H A N G M A N # ${maxAttempts} attempts\n`);
 
-    console.log();
-}
+    do {
+        choice = input(m);
 
-console.log(
-    guesses.includes('-')
-        ? 'You lost!'
-        : `You guessed the word ${answer}!\nYou survived!`
-);
+        switch (choice) {
+            case 'play':
+                playGame();
+                break;
+            case 'results':
+                showResults();
+                break;
+            case 'exit':
+                keepGoing = false;
+                break;
+            default:
+                console.log('Invalid option!\n');
+                break;
+        }
+    } while (keepGoing);
+
+    console.log('\nThank you for playing Hangman!!');
+}());
